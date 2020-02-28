2195431f9e64bd5f3fc773551f615364de905347
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-02-28 21:22:55 +0700

    Updates

---
+++ precache-manifest.204a44b98b7b228b9caa25044751686e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10ff747de51c882af026b9c2c001868",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "2c76f7610303a3536723",
+    "url": "/app/static/css/main.5ff8e865.chunk.css"
+  },
+  {
+    "revision": "6862bbecc70c4546d80d",
+    "url": "/app/static/js/2.c8d5c34e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/app/static/js/2.c8d5c34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c76f7610303a3536723",
+    "url": "/app/static/js/main.0d349143.chunk.js"
+  },
+  {
+    "revision": "499fd37f5a06f40e00cb",
+    "url": "/app/static/js/runtime-main.5e4ac1dc.js"
+  },
+  {
+    "revision": "d8d27c3495d7cedffe3d1c72886b82bc",
+    "url": "/app/static/media/cleaning_services.d8d27c34.svg"
+  },
+  {
+    "revision": "b801e345b48fcc879ac42d11c952cfa5",
+    "url": "/app/static/media/cleansheet-owner.b801e345.png"
+  },
+  {
+    "revision": "b6b4e62593d693c14ee8e9f304f5bb66",
+    "url": "/app/static/media/env_cleaning.b6b4e625.svg"
+  },
+  {
+    "revision": "1c3a31877bcadd8c9f6ca5e27fd34300",
+    "url": "/app/static/media/ipb-university.1c3a3187.png"
+  },
+  {
+    "revision": "484959a745828c0b6f99cf318ca9767b",
+    "url": "/app/static/media/landingpage_main_image.484959a7.svg"
+  },
+  {
+    "revision": "e6387b253f454df1c33f999b7589530a",
+    "url": "/app/static/media/laundry_washing.e6387b25.svg"
+  },
+  {
+    "revision": "f04e8ad698c6147fd3be121521a1e575",
+    "url": "/app/static/media/logo-bogor.f04e8ad6.png"
+  },
+  {
+    "revision": "f3864510e321a3938357b1f7705f5d12",
+    "url": "/app/static/media/logo_cs.f3864510.png"
+  },
+  {
+    "revision": "353acf0599208c640ceeefa3745612f1",
+    "url": "/app/static/media/logo_cs_abu.353acf05.png"
+  },
+  {
+    "revision": "206a5b3bb89dd8c62c953f3cc1a53faf",
+    "url": "/app/static/media/prod1.206a5b3b.png"
+  },
+  {
+    "revision": "ea0a5ddc73b0add0c0b82ca86f99f9ce",
+    "url": "/app/static/media/whatsapp.ea0a5ddc.svg"
+  }
+]);
